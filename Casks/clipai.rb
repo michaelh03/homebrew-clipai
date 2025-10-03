95821e4c385e116418e21ba3eadffb923424d570
Michael Hait <mhait@paloaltonetworks.com>
2025-10-03 10:42:35 +0300

    add clipai casks

---
+++ Casks/clipai.rb
@@ -0,0 +1,31 @@
+cask "clipai" do
+  version "1.0.0"  # Update with your actual version
+  sha256 "9b6b03de850cce78cbdf816b66847781e1a597bd010242aa8a7af2c42c184e90"  # Calculate this from your .dmg or .zip
+
+  url "https://github.com/michaelh03/ClipAi/releases/download/ClipAi-1.0.0/ClipAi-1.0.0.dmg"
+  name "ClipAi"
+  desc " Clipboard manager with sparkle of AI ✨"  # Add your app description
+  homepage "https://github.com/michaelh03/ClipAi"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "ClipAi.app"
+
+  # If your app requires specific permissions or setup
+  # postflight do
+  #   system_command "/usr/bin/open",
+  #                  args: ["-a", "ClipAi"]
+  # end
+
+  # Uninstall script if needed
+  # uninstall quit: "com.yourcompany.clipai"
+
+  zap trash: [
+    "~/Library/Application Support/ClipAI",
+    "~/Library/Preferences/com.michaelh.clipai.plist",
+    "~/Library/Caches/com.michaelh.clipai",
+  ]
+end
