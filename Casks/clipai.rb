2d383905924a03c57d68debeb4566a28ce8a4fa0
Michael Hait <mhait@paloaltonetworks.com>
2026-01-16 17:18:22 +0200

    bump to 1.0.3

--- Casks/clipai.rb
+++ Casks/clipai.rb
@@ -1,8 +1,8 @@
 cask "clipai" do
-  version "1.0.1"  # Update with your actual version
-  sha256 "dad87ea96d1a0e61493f79c02c8d77256ef83d999bf59f43f0c9560bad601e3d"  # Calculate this from your .dmg or .zip
+  version "1.0.3"  # Update with your actual version
+  sha256 "6f08d94b5eca71248d33d96581d849a0edee0fe8c26b83f3a876b0a0458c789d"  # Calculate this from your .dmg or .zip
 
-  url "https://github.com/michaelh03/ClipAi/releases/download/ClipAi-1.0.1/ClipAI-1.0.1.dmg"
+  url "https://github.com/michaelh03/ClipAi/releases/download/ClipAi-1.0.3/ClipAI-1.0.3.dmg"
   name "ClipAi"
   desc " Clipboard manager with sparkle of AI ✨"  # Add your app description
   homepage "https://github.com/michaelh03/ClipAi"
